4e294af994a07f3dbd671c5fd3c03389abda0ec1
somecallmetim <timbauer@ymail.com>
2016-01-10 12:18:32 -0800

    Added basic ScreenOverlayStack class. Feature still needs a lot of work

---
+++ src/ScreenOverlayStack.java
@@ -0,0 +1,38 @@
+/**
+ * Created by timbauer on 1/10/16.
+ */
+
+import javax.swing.*;
+import java.util.Stack;
+
+public class ScreenOverlayStack extends Stack<JPanel> {
+
+    private static ScreenOverlayStack screenOverlayStack = new ScreenOverlayStack();
+    private JPanel currentScreen = null;
+
+    private ScreenOverlayStack() {
+    }
+
+    public static ScreenOverlayStack getScreenOverlayStack() {
+        return screenOverlayStack;
+    }
+
+    @Override
+    public JPanel push(JPanel newPanel){
+        if(currentScreen != null){
+            super.push(currentScreen);
+            currentScreen = newPanel;
+        }else {
+            currentScreen = newPanel;
+        }
+        return newPanel;
+    }
+
+    @Override
+    public JPanel pop(){
+        currentScreen = super.pop();
+        return currentScreen;
+    }
+
+
+}
